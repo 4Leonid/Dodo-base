398a30e321633d487f595549ed9e7307ce8667f6
Leo <leo-turko@mail.ru>
2024-10-07 16:45:05 +0300

    Add main branch

---
+++ Dodo-base/Dodo-base/Modules/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dodo-base
+//
+//  Created by Леонид Турко on 07.10.2024.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .orange
+  }
+
+
+}
+
